73edf18fd69244f930626a1b16e42e50ccbdf744
Romain Gueffier <r.gueffier.pro@gmail.com>
2022-09-09 19:03:36 +0200

    feat(theme): Add dark mode force toggle, pdf french cv and fix issues with prettier for css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   plugins: [],
   mode: 'jit',
+  darkMode: 'class',
   theme: {
     extend: {
       keyframes: {
