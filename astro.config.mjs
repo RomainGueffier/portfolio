36116c0ccfa67da9a7b2f026ce19745cb74996fc
romain-g <r.gueffier.pro@gmail.com>
2022-10-23 23:43:27 +0200

    upgrade packages to astro 1.5 and fix some minor responsive issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import react from '@astrojs/react'
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
-  adapter: node(),
+  adapter: node({ mode: 'standalone' }),
 })
