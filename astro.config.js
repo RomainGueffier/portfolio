f2e63f78cb08c674645ef46edf6da2e91992a937
Romain Gueffier <r.gueffier.pro@gmail.com>
2023-01-06 20:43:10 +0100

    switch node adaptater to vercel one

--- astro.config.js
+++ astro.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import node from '@astrojs/node'
 import react from '@astrojs/react'
+
+// https://astro.build/config
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
 })
