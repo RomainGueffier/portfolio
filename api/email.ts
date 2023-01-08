c4fbf55cc623ffebee2458e20d60095ef6817afb
Romain Gueffier <r.gueffier.pro@gmail.com>
2023-01-08 18:17:57 +0100

    disable cors temporarely

--- api/email.ts
+++ api/email.ts
@@ -1,12 +1,12 @@
 import { Mailer } from '../src/libs/ssr/mailer'
-import allowCors from '../src/libs/ssr/cors'
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-async function handler(request: VercelRequest, response: VercelResponse) {
+export default async function handler(
+  request: VercelRequest,
+  response: VercelResponse
+) {
   const data = await request.body
   const result = await Mailer.sendContactEmail(data)
   console.log(result)
   response.status(200).json('success')
 }
-
-export default allowCors(handler)
